69104df1ed624c58c8ead5574038f1d7861c93fa
SATYAM SHAH <143245535+satyam-8092@users.noreply.github.com>
2025-05-16 10:37:31 +0530

    Time: 17 ms (8.63%), Space: 23.9 MB (10%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int xorr = 0;
-        for(int it:nums){
-            xorr=xorr^it;
+         unordered_map<int, int> freq;
+        for (int num : nums) {
+            freq[num]++;
         }
-        return xorr;
+        for (auto [key, value] : freq) {
+            if (value == 1)
+                return key;
+        }
+        return -1;
     }
 };
