20bccdac362a9e043d9083d9836f2a2fd214fcf3
SATYAM SHAH <143245535+satyams8092@users.noreply.github.com>
2025-07-29 20:11:36 +0530

    Time: 0 ms (100%), Space: 8.6 MB (20.82%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -4,8 +4,7 @@ public:
         int y=0;
         while(x!=0){
             int r=x%10;
-            if (y > INT_MAX/10 ) return 0;
-            if (y < INT_MIN/10) return 0;
+            if ((y > INT_MAX/10) || (y < INT_MIN/10) ) return 0;
             x=x/10;
             y=(y*10)+r;
         }
