917e7b411c29366d3d8acdb661c970606c29f53e
SATYAM SHAH <143245535+satyams8092@users.noreply.github.com>
2025-09-15 14:57:33 +0530

    Time: 0 ms (100%), Space: 8.8 MB (80.49%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
+    //brute appraoch
     string removeOuterParentheses(string s) {
-        int cnt=0;
-        string ans="";
-        for(int i=0;i<s.length();i++){
-            if(s.at(i)==')'){
-                cnt--;
-            }
-            if(cnt!=0){
-                ans.push_back(s.at(i));
-            }
-            if(s.at(i)=='('){
-                cnt++;
+        stack<char> st;
+        string res = "";
+        for (char c : s) {
+            if (c == '(') {
+                if (!st.empty()) res += c;
+                st.push(c);
+            } else {
+                st.pop();
+                if (!st.empty()) res += c;
             }
         }
-        return ans;
+        return res;
     }
 };
