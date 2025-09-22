71dd6ed6e6e47b499ff0bc19929d23ae4955bd09
SATYAM SHAH <143245535+satyams8092@users.noreply.github.com>
2025-09-22 15:22:20 +0530

    Time: 11 ms (26.16%), Space: 12.5 MB (61.2%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -13,14 +13,14 @@ public:
         int result=0;
         int n=s.length();
         
-        for(int i=0;i<n-1;i++){
+        for(int i=0;i<n;i++){
             if(mp[s[i]]>=mp[s[i+1]]){
                 result+=mp[s[i]];
             }else{
                 result-=mp[s[i]];
             }
         }
-        result+=mp[s[n-1]];
+        // result+=mp[s[n-1]];
         return result;
     }
 };
