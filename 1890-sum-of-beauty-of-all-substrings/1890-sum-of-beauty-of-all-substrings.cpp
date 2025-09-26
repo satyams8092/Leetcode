02b28d1085b2e16bea3358207e27184449bebb13
SATYAM SHAH <143245535+satyams8092@users.noreply.github.com>
2025-09-26 22:49:30 +0530

    Time: 55 ms (77.04%), Space: 11.2 MB (47.15%) - LeetHub

--- 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.cpp
+++ 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.cpp
@@ -4,30 +4,36 @@ public:
         int n = s.length();
         int totalBeauty = 0;
         
-        // Check all possible substrings
         for (int i = 0; i < n; i++) {
             vector<int> cnt(26, 0);
             
             for (int j = i; j < n; j++) {
-                // Add current character to frequency count
                 cnt[s[j] - 'a']++;
                 
-                // Calculate beauty for current substring s[i...j]
-                int most = 0;
-                int least = INT_MAX;
-                
-                for (int freq : cnt) {
-                    if (freq > 0) {  // Only consider characters that appear
-                        most = max(most, freq);
-                        least = min(least, freq);
-                    }
+                // Early termination: if substring length < 3, beauty is always 0
+                if (j - i < 2) {
+                    continue;
                 }
                 
-                // Add beauty to total (difference between max and min frequency)
-                totalBeauty += (most - least);
+                totalBeauty += calculateBeauty(cnt);
             }
         }
         
         return totalBeauty;
+    }
+    
+private:
+    int calculateBeauty(const vector<int>& cnt) {
+        int most = 0;
+        int least = INT_MAX;
+        
+        for (int freq : cnt) {
+            if (freq > 0) {
+                most = max(most, freq);
+                least = min(least, freq);
+            }
+        }
+        
+        return most - least;
     }
 };
