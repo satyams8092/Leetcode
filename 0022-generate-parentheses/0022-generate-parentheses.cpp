0312192d854af5383ea937b365e6df0860890876
SATYAM SHAH <143245535+satyams8092@users.noreply.github.com>
2026-07-05 23:04:19 +0530

    Time: 3 ms (70.28%), Space: 13.1 MB (79.65%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -2,40 +2,26 @@ class Solution {
 public:
     vector<string> result;
 
-    bool isValid(string& str){
-        int cnt=0;
-
-        for(char &ch:str){
-            if(ch=='(')
-                cnt++;
-            else
-                cnt--;
-            if(cnt<0){
-                return false;
-            }
-        }
-        return cnt==0;
-    }
-
-    void solve(string& curr,int n){
+    void solve(string& curr,int n,int open,int close){
         if(curr.length()==2*n){
-            if(isValid(curr)){
-                result.push_back(curr);
-            }
+            result.push_back(curr);
             return;
         }
+        if(open < n){
+            curr.push_back('(');
+            solve(curr,n,open+1,close);
+            curr.pop_back();
+        }
 
-        curr.push_back('(');
-        solve(curr,n);
-        curr.pop_back();
-
-        curr.push_back(')');
-        solve(curr,n);
-        curr.pop_back();
+        if(close < open){
+            curr.push_back(')');
+            solve(curr,n,open,close+1);
+            curr.pop_back();
+        }
     }
     vector<string> generateParenthesis(int n) {
         string curr="";
-        solve(curr,n);
+        solve(curr,n,0,0);
         return result;
     }
 };
