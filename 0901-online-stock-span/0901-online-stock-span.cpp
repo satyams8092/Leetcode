e7fee2ed72f585b06ca3555f375927c0e56c9fb2
SATYAM SHAH <143245535+satyams8092@users.noreply.github.com>
2026-09-24 02:03:04 +0530

    Time: 29 ms (69.63%), Space: 97.2 MB (26.08%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.cpp
+++ 0901-online-stock-span/0901-online-stock-span.cpp
@@ -1,20 +1,19 @@
 class StockSpanner {
 public:
-    vector<int> result;
+    stack<pair<int,int>> st;
+    int ind=-1;
     StockSpanner() {
+        
     }
     
     int next(int price) {
-        result.push_back(price);
-        int cnt=1;
-        for(int i=result.size()-2;i>=0;i--){
-            if(result[i]<=price){
-                cnt++;
-            }else{
-                break;
-            }
+        ind++;
+        while(!st.empty() && st.top().first<=price){
+            st.pop();
         }
-        return cnt;
+        int ans=ind-(!st.empty() ? st.top().second : -1);
+        st.push({price,ind});
+        return ans;
     }
 };
 
